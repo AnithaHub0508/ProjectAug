0a92b57b5b37f947f1cc87ff6c6e034aa2b38e68
AnithaHub0508 <anitharaj032@gmail.com>
2019-08-15 11:01:55 +0530

    new code added for poly

---
+++ src/org/cts/login/Diffpack.java
@@ -0,0 +1,9 @@
+package org.cts.login;
+
+public class Diffpack {
+	public void clientId() {
+		System.out.println("1234");
+
+	}
+
+}
---
+++ src/prac/ani/login/Java.java
@@ -0,0 +1,18 @@
+package prac.ani.login;
+
+import org.cts.login.Diffpack;
+
+public class Java {
+	public void empId() {
+System.out.println("anitha");
+	}
+	public static void main(String[] args) {
+		Java a=new Java();
+		a.empId();
+		Diffpack d=new Diffpack();
+		d.clientId();
+		
+	}
+
+}
+
